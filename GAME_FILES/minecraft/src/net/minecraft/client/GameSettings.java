18b3dc42e8625476b1a008b4a7ac44c014397f4d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2024-07-11 12:03:13 -0400

    Refactor GameSettings; Make E default inventory key

--- GAME_FILES/minecraft/src/net/minecraft/client/GameSettings.java
+++ GAME_FILES/minecraft/src/net/minecraft/client/GameSettings.java
@@ -8,8 +8,16 @@ import java.io.PrintWriter;
 import org.lwjgl.input.Keyboard;
 
 public final class GameSettings {
-	private static final String[] RENDER_DISTANCES = new String[]{"FAR", "NORMAL", "SHORT", "TINY"};
-	private static final String[] DIFFICULTIES = new String[]{"Peaceful", "Easy", "Normal", "Hard"};
+	
+	private static final String[] RENDER_DISTANCES = new String[]{ "Far", "Normal", "Short", "Tiny" };
+	private static final String[] DIFFICULTIES = new String[]{ "Peaceful", "Easy", "Normal", "Hard" };
+	
+	private Minecraft mc;
+	private File optionsFile;
+	
+	public int numberOfOptions = 9;
+	public int difficulty = 2;
+	public boolean thirdPersonView = false;
 	public boolean music = true;
 	public boolean sound = true;
 	public boolean invertMouse = false;
@@ -18,84 +26,98 @@ public final class GameSettings {
 	public boolean fancyGraphics = true;
 	public boolean anaglyph = false;
 	public boolean limitFramerate = false;
+	
 	public KeyBinding keyBindForward = new KeyBinding("Forward", 17);
 	public KeyBinding keyBindLeft = new KeyBinding("Left", 30);
 	public KeyBinding keyBindBack = new KeyBinding("Back", 31);
 	public KeyBinding keyBindRight = new KeyBinding("Right", 32);
 	public KeyBinding keyBindJump = new KeyBinding("Jump", 57);
-	public KeyBinding keyBindInventory = new KeyBinding("Inventory", 23);
+	public KeyBinding keyBindInventory = new KeyBinding("Inventory", 18);
 	public KeyBinding keyBindDrop = new KeyBinding("Drop", 16);
 	public KeyBinding keyBindChat = new KeyBinding("Chat", 20);
 	public KeyBinding keyBindToggleFog = new KeyBinding("Toggle fog", 33);
 	public KeyBinding keyBindSave = new KeyBinding("Save location", 28);
 	public KeyBinding keyBindLoad = new KeyBinding("Load location", 19);
+	
 	public KeyBinding[] keyBindings = new KeyBinding[]{this.keyBindForward, this.keyBindLeft, this.keyBindBack, this.keyBindRight, this.keyBindJump, this.keyBindDrop, this.keyBindInventory, this.keyBindChat, this.keyBindToggleFog, this.keyBindSave, this.keyBindLoad};
-	private Minecraft mc;
-	private File optionsFile;
-	public int numberOfOptions = 9;
-	public int difficulty = 2;
-	public boolean thirdPersonView = false;
 
-	public GameSettings(Minecraft var1, File var2) {
-		this.mc = var1;
-		this.optionsFile = new File(var2, "options.txt");
+	public GameSettings(Minecraft mc, File fileRoot) {
+		this.mc = mc;
+		this.optionsFile = new File(fileRoot, "options.txt");
 		this.loadOptions();
 	}
 
-	public final String setKeyBindingString(int var1) {
-		return this.keyBindings[var1].keyDescription + ": " + Keyboard.getKeyName(this.keyBindings[var1].keyCode);
+	public final String setKeyBindingString(int keyBindID) {
+		return this.keyBindings[keyBindID].keyDescription + ": " + Keyboard.getKeyName(this.keyBindings[keyBindID].keyCode);
 	}
 
-	public final void setKeyBinding(int var1, int var2) {
-		this.keyBindings[var1].keyCode = var2;
+	public final void setKeyBinding(int keyBindID, int keyCode) {
+		this.keyBindings[keyBindID].keyCode = keyCode;
 		this.saveOptions();
 	}
 
-	public final void setOptionValue(int var1, int var2) {
-		if(var1 == 0) {
-			this.music = !this.music;
-			this.mc.sndManager.onSoundOptionsChanged();
-		}
-
-		if(var1 == 1) {
-			this.sound = !this.sound;
-			this.mc.sndManager.onSoundOptionsChanged();
-		}
-
-		if(var1 == 2) {
-			this.invertMouse = !this.invertMouse;
-		}
-
-		if(var1 == 3) {
-			this.showFPS = !this.showFPS;
-		}
-
-		if(var1 == 4) {
-			this.renderDistance = this.renderDistance + var2 & 3;
-		}
-
-		if(var1 == 5) {
-			this.fancyGraphics = !this.fancyGraphics;
-		}
-
-		if(var1 == 6) {
-			this.anaglyph = !this.anaglyph;
-			this.mc.renderEngine.refreshTextures();
-		}
-
-		if(var1 == 7) {
-			this.limitFramerate = !this.limitFramerate;
-		}
-
-		if(var1 == 8) {
-			this.difficulty = this.difficulty + var2 & 3;
+	public final void setOptionValue(int optionID, int value) {
+		
+		switch (optionID) {
+		
+			case 0:
+				this.music = !this.music;
+				this.mc.sndManager.onSoundOptionsChanged();
+				break;
+				
+			case 1:
+				this.sound = !this.sound;
+				this.mc.sndManager.onSoundOptionsChanged();
+				break;
+				
+			case 2:
+				this.invertMouse = !this.invertMouse;
+				break;
+				
+			case 3:
+				this.showFPS = !this.showFPS;
+				break;
+				
+			case 4:
+				this.renderDistance = this.renderDistance + value & 3;
+				break;
+				
+			case 5:
+				this.fancyGraphics = !this.fancyGraphics;
+				break;
+				
+			case 6:
+				this.anaglyph = !this.anaglyph;
+				this.mc.renderEngine.refreshTextures();
+				break;
+				
+			case 7:
+				this.limitFramerate = !this.limitFramerate;
+				break;
+				
+			case 8:
+				this.difficulty = this.difficulty + value & 3;
+				break;
 		}
 
 		this.saveOptions();
 	}
 
-	public final String setOptionString(int var1) {
-		return var1 == 0 ? "Music: " + (this.music ? "ON" : "OFF") : (var1 == 1 ? "Sound: " + (this.sound ? "ON" : "OFF") : (var1 == 2 ? "Invert mouse: " + (this.invertMouse ? "ON" : "OFF") : (var1 == 3 ? "Show FPS: " + (this.showFPS ? "ON" : "OFF") : (var1 == 4 ? "Render distance: " + RENDER_DISTANCES[this.renderDistance] : (var1 == 5 ? "View bobbing: " + (this.fancyGraphics ? "ON" : "OFF") : (var1 == 6 ? "3d anaglyph: " + (this.anaglyph ? "ON" : "OFF") : (var1 == 7 ? "Limit framerate: " + (this.limitFramerate ? "ON" : "OFF") : (var1 == 8 ? "Difficulty: " + DIFFICULTIES[this.difficulty] : ""))))))));
+	public final String setOptionString(int optionID) {
+		
+		switch (optionID) {
+			case 0: return "Music: " + (this.music ? "ON" : "OFF");
+			case 1: return "Sound: " + (this.sound ? "ON" : "OFF");
+			case 2: return "Invert mouse: " + (this.invertMouse ? "ON" : "OFF");
+			case 3: return "Show debug: " + (this.showFPS ? "ON" : "OFF");
+			case 4: return "Render distance: " + RENDER_DISTANCES[this.renderDistance];
+			case 5: return "View bobbing: " + (this.fancyGraphics ? "ON" : "OFF");
+			case 6: return "3d anaglyph: " + (this.anaglyph ? "ON" : "OFF");
+			case 7: return "Limit framerate: " + (this.limitFramerate ? "ON" : "OFF");
+			case 8: return "Difficulty: " + DIFFICULTIES[this.difficulty];
+			default: return "";
+		}
+		
 	}
 
 	private void loadOptions() {
@@ -162,25 +184,26 @@ public final class GameSettings {
 
 	public final void saveOptions() {
 		try {
-			PrintWriter var1 = new PrintWriter(new FileWriter(this.optionsFile));
-			var1.println("music:" + this.music);
-			var1.println("sound:" + this.sound);
-			var1.println("invertYMouse:" + this.invertMouse);
-			var1.println("showFrameRate:" + this.showFPS);
-			var1.println("viewDistance:" + this.renderDistance);
-			var1.println("bobView:" + this.fancyGraphics);
-			var1.println("anaglyph3d:" + this.anaglyph);
-			var1.println("limitFramerate:" + this.limitFramerate);
-			var1.println("difficulty:" + this.difficulty);
+			PrintWriter out = new PrintWriter(new FileWriter(this.optionsFile));
+			out.println("music:" + this.music);
+			out.println("sound:" + this.sound);
+			out.println("invertYMouse:" + this.invertMouse);
+			out.println("showFrameRate:" + this.showFPS);
+			out.println("viewDistance:" + this.renderDistance);
+			out.println("bobView:" + this.fancyGraphics);
+			out.println("anaglyph3d:" + this.anaglyph);
+			out.println("limitFramerate:" + this.limitFramerate);
+			out.println("difficulty:" + this.difficulty);
 
-			for(int var2 = 0; var2 < this.keyBindings.length; ++var2) {
-				var1.println("key_" + this.keyBindings[var2].keyDescription + ":" + this.keyBindings[var2].keyCode);
+			for(int i = 0; i < this.keyBindings.length; i++) {
+				out.println("key_" + this.keyBindings[i].keyDescription + ":" + this.keyBindings[i].keyCode);
 			}
 
-			var1.close();
-		} catch (Exception var3) {
+			out.close();
+			
+		} catch (Exception e) {
 			System.out.println("Failed to save options");
-			var3.printStackTrace();
+			e.printStackTrace();
 		}
 	}
 }
